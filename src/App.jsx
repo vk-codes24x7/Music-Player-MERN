1cc1f73323ebefe93c993cc611c9a0d631387a7b
vk-codes24x7 <vinayakrishna1001@gmail.com>
2024-02-09 13:08:56 -0500

    Added the pages folder and the index file for the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,5 @@
 import { UseSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+
+import { SearchBar, SideBar, MusicPlayer, TopPlay } from "./components";
+import {} from "./pages";
